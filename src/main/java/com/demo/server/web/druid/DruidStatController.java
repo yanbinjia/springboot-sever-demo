aa9d4fb932328b639f8d8d11ee220ddeb5338e03
yanbinjia <yanbinjia@126.com>
2020-06-02 19:49:52 +0800

    sdf

--- src/main/java/com/demo/server/web/druid/DruidStatController.java
+++ src/main/java/com/demo/server/web/druid/DruidStatController.java
@@ -1,4 +1,4 @@
-package com.demo.server.web;
+package com.demo.server.web.druid;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
