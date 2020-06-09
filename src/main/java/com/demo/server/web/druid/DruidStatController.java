4e64acddd8151e11b615b60099dc2751cae73bc4
yanbinjia <yanbinjia@126.com>
2020-06-09 10:24:40 +0800

    d

--- src/main/java/com/demo/server/web/druid/DruidStatController.java
+++ src/main/java/com/demo/server/web/druid/DruidStatController.java
@@ -5,10 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.druid.stat.DruidStatManagerFacade;
+import com.demo.server.interceptor.SignPass;
+import com.demo.server.interceptor.TokenPass;
 
 @RestController
 @RequestMapping("/druid")
 public class DruidStatController {
+	@SignPass
+	@TokenPass
 	@GetMapping("/stat")
 	public Object druidStat() {
 		return DruidStatManagerFacade.getInstance().getDataSourceStatDataList();
