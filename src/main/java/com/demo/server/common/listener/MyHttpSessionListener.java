8842958824d9b6bb961bd5fa5b87f4e9fca87b3f
yanbinjia <yanbinjia@126.com>
2020-07-22 23:04:12 +0800

    some optimize.

--- src/main/java/com/demo/server/common/listener/MyHttpSessionListener.java
+++ src/main/java/com/demo/server/common/listener/MyHttpSessionListener.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2020 demo ^-^.
+ * @Author: yanbinjia@126.com
+ * @LastModified: 2020-07-22T23:03:27.071+08:00
+ */
+
 package com.demo.server.common.listener;
 
 import lombok.extern.slf4j.Slf4j;
