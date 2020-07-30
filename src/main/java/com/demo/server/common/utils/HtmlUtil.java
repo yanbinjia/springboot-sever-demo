5bf2856e6966eae46d1e694b25d30a453c0d3bb2
yanbinjia <yanbinjia@126.com>
2020-07-30 16:15:05 +0800

    Add system oshi util and test

---
+++ src/main/java/com/demo/server/common/utils/HtmlUtil.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020 demo ^-^.
+ * @Author: yanbinjia@126.com
+ * @LastModified: 2020-07-30T14:08:12.932+08:00
+ */
+
+package com.demo.server.common.utils;
+
+import org.springframework.web.util.HtmlUtils;
+
+public class HtmlUtil {
+    public static final String CHARSET_UTF8 = "UTF-8";
+
+    public static String htmlEscape(String value) {
+        return HtmlUtils.htmlEscape(value, CHARSET_UTF8);
+    }
+
+    public static String htmlUnescape(String value) {
+        return HtmlUtils.htmlUnescape(value);
+    }
+
+    /**
+     * 过滤HTML文本，防止XSS攻击
+     *
+     * @param htmlContent HTML内容
+     * @return 过滤后的内容
+     */
+    public static String filter(String htmlContent) {
+        return new HtmlFilter().filter(htmlContent);
+    }
+
+}
